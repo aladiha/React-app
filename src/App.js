7da27ebf8946918a1537f1ff903efd113d7b1705
aladiha <34811841+aladiha@users.noreply.github.com>
2020-04-10 02:25:00 +0300

    aa

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
    return (
     <div className="App">
 
-     sadasd
+     
     </div>
   );
   return React.createElement('h1', { className: 'App' }, 'Hi i\'am React al sa app!!');
